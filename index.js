8e5b1affbb0559e555d2fb1a259026d2a0bfeb8c
KarolGonzalezru-77 <karolgonzalezru@amigo.edu.co>
2026-05-07 00:09:39 -0500

    chore: add vercel configuration

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const app = require('./src/app');
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`Peluquería Canina App running on http://localhost:${port}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(port, () => {
+        console.log(`Peluquería Canina App running on http://localhost:${port}`);
+    });
+}
+
+module.exports = app;
